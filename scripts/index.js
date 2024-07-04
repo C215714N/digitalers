3b1ca9bcd276a51d74705ee28ac9aabb23ce7dab
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-07-04 20:04:05 -0300

    implementar logica mapa de contacto

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 /* Declaraciones */
 var menuButton = document.getElementById('menu-button');
 var menuList = document.getElementById('menu-list');
+var mapFrame = document.querySelector('iframe.map');
+var mapButton = document.querySelector('button.map');
 
 /* Funciones */
 function toggle(element,className="active"){
@@ -9,3 +11,4 @@ function toggle(element,className="active"){
 
 /* Eventos */
 menuButton.onclick = () => toggle(menuList)
+mapButton.onclick = () => toggle(mapFrame)
