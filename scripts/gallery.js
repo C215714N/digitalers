372adb9da6b0c878196aef78ec9dd5e10829aa03
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-08-01 20:59:40 -0300

    agregar transiciones y mapeo de elementos

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -15,7 +15,27 @@ const getActiveItem = (array, type, className="active") => {
                 nextItem = i < array.length - 1 ? array[i+1] : array[0]
                 break;
                 default: 
-                    nextItem = array[type]
+                nextItem = array[type]
         } } } 
     nextItem.classList.add(className);
 }
+// Iteracion del Array de galerias
+galleries.forEach(gallery => {
+    // Identificar elementos de Cada galeria
+    const images = gallery.querySelectorAll(".gallery-item");
+    const controls = gallery.querySelectorAll(".controls button");
+    const buttons = ["prev", "next"]
+    // Avanzar y Retroceder
+    buttons.forEach(btn => {
+        const button = gallery.querySelector(`.${btn}`);
+        button.addEventListener("click", () => {
+            getActiveItem(images, btn);
+            getActiveItem(controls, btn);
+        })
+    })
+    // Boton de Seleccion
+    controls.forEach((ctrl,index) => ctrl.addEventListener("click", () => {
+        getActiveItem(images,index)
+        getActiveItem(controls,index)
+    }))
+})
