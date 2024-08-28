6066bebb538608f6e2075e34eecdaf6570b60c4f
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-08-27 22:08:55 -0300

    actualizar constantes globales y contenido principal

--- app/index.js
+++ app/index.js
@@ -1,10 +1,16 @@
 import { getData } from "./async.js";
 import { root, main, links, brand, pub, jph } from "./variables.js";
 import Nav from "./components/navigation.js";
+// Componentes
+import { Post } from "./components/post.js";
+import { User } from "./components/user.js";
+import { Profile } from "./components/profile.js";
+import { Album } from "./components/album.js";
+import { Todo } from "./components/todo.js";
 
 root.innerHTML+= Nav(links, brand);
 
-document.addEventListener('click', (ev) => {
+document.addEventListener('click', async (ev) => {
     ev.preventDefault();
     const tag = ev.target.tagName;
     if(tag === "A"){
@@ -12,19 +18,31 @@ document.addEventListener('click', (ev) => {
     }
     if(tag === "BUTTON"){
         if(ev.target.hasAttribute('data-url')){
-            const { url, link } = ev.target.dataset
+            const { url, link, component } = ev.target.dataset
             const server = link === "remote" ? jph : link === "local" ? pub : null;
             history.pushState("","", url);
-            const data = server ? getData(`${server}/${url}`) : null
-
-            render(data, url)
+            const data = server && await getData(`${server}/${url}`)
+            render(data, component)
         }
     }
 })
 
-function render(){
-    const content = document.createDocumentFragment();
-    
-    main.appendChild(content)
+async function render(data, fn){
+    Object.assign(main, {
+        className: "row container gap-2 mx-auto p-3",
+        innerHTML: ""
+    })
+    // Verificamos si es un array de datos y luego lo recorremos
+    if (Array.from(data).length >= 1){
+        data.map(async (d,i) => {
+            const currentData = await eval(`${fn}(d)`);
+            main.innerHTML+=currentData;
+            if ((i + 1) % 3 == 0) main.innerHTML+='<div class="w-100">';
+    })}
+    // En caso de ser un Objeto lo renderizamos directamente
+    else {
+        const currentData = await eval(`${fn}(data)`)
+        main.innerHTML = currentData;
+    }
     root.appendChild(main)
 }
