947f05215ff02f21237f0a4332cbb9f9171d83fc
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-19 20:25:45 -0300

    declaracion de variables para headers y contenido

--- index.js
+++ index.js
@@ -1,35 +1,62 @@
 import { createServer } from 'http'
+
 const PORT = process.env.PORT ?? 3000
 const HOST = process.env.HOST ?? "localhost"
 const app = createServer( (req, res) => {
+    let 
+        contentType = 'text', 
+        status = 200, 
+        content;
     const { url, method } = req
     if (method === "GET"){
         switch(url){
             case "/":
             case "/index":
             case "/index.html":
-                res.write("<h1>Bienvenido</h1>")
+                contentType = 'text/html'
+                content = "<h1>Bienvenido</h1>"
             break;
             case "/css":
             case "/styles":
             case "/styles.css":
             case "/default.css":
-                res.write("body{ margin: 0 }")
+                contentType='text/css'
+                content = `
+                    * {
+                        box-sizing: border-box;
+                        scroll-behavior: smooth;
+                        font-family: Raleway, calibri, sans-serif;    
+                    }
+                    body {
+                        margin: 0 
+                    }`
             break;
             case "/js":
             case "/scripts":
             case "/app":
             case "/app.js":
             case "/index.js":
-                res.write("document.addEventListener('DOMContentLoaded', ()=> console.log('pagina cargada'))")
+                contentType ='text/js'
+                content =`
+                document.addEventListener('DOMContentLoaded', ()=> {
+                    console.log('pagina cargada')
+                })`
             break;
             default:
-                res.write('Error 404: Ruta no Encontrada')
+                status = 404
+                contentType= "application/json"
+                content = JSON.stringify({
+                    code: 404,
+                    message: "Not Found",
+                    recomendation: "try another route",
+                })
         }
     }
     if (method === "POST"){
         res.write("enviaste datos")
     }
+    res.writeHead(status, "OK", {'Content-Type': contentType})
+    res.write(content)
     res.end()
 })
 
