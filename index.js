9c6a976afe0ed7527d47de88f3b2b64d1059fd1b
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-19 21:25:59 -0300

    servidor con rutas estaticas y dinamicas

--- index.js
+++ index.js
@@ -1,63 +1,27 @@
+import { join, extname } from 'path'
+import { readFile } from 'fs'
 import { createServer } from 'http'
+
+import { getContent } from './src/content-types.js'
+import { getFile } from './src/paths.js'
 
 const PORT = process.env.PORT ?? 3000
 const HOST = process.env.HOST ?? "localhost"
 const app = createServer( (req, res) => {
-    let 
-        contentType = 'text', 
-        status = 200, 
-        content;
+    let content, status=200;
     const { url, method } = req
-    if (method === "GET"){
-        switch(url){
-            case "/":
-            case "/index":
-            case "/index.html":
-                contentType = 'text/html'
-                content = "<h1>Bienvenido</h1>"
-            break;
-            case "/css":
-            case "/styles":
-            case "/styles.css":
-            case "/default.css":
-                contentType='text/css'
-                content = `
-                    * {
-                        box-sizing: border-box;
-                        scroll-behavior: smooth;
-                        font-family: Raleway, calibri, sans-serif;    
-                    }
-                    body {
-                        margin: 0 
-                    }`
-            break;
-            case "/js":
-            case "/scripts":
-            case "/app":
-            case "/app.js":
-            case "/index.js":
-                contentType ='text/js'
-                content =`
-                document.addEventListener('DOMContentLoaded', ()=> {
-                    console.log('pagina cargada')
-                })`
-            break;
-            default:
-                status = 404
-                contentType= "application/json"
-                content = JSON.stringify({
-                    code: 404,
-                    message: "Not Found",
-                    recomendation: "try another route",
-                })
+    const publicDir = process.cwd() + "/public/";
+    const file = getFile(url)
+    const filePath = join(publicDir + file)
+    const headers = getContent(extname(file));
+    method === "GET" && readFile(filePath, (err, data) => {
+        if (err) {
+            status = 404,
+            content = "Error 404 Archivo no encontrado"
         }
-    }
-    if (method === "POST"){
-        res.write("enviaste datos")
-    }
-    res.writeHead(status, "OK", {'Content-Type': contentType})
-    res.write(content)
-    res.end()
+        else content = data.toLocaleString();
+        res.writeHead(status,'', headers)
+        res.end(content)
+    })
 })
-
 app.listen(PORT, HOST, console.log("service running"))
