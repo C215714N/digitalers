80527ad4339ee4b57bef25f5c8cf2d6f4156e96a
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-24 21:18:20 -0300

    datos de ejemplo para usuarios

---
+++ app/models/userModel.js
@@ -0,0 +1,37 @@
+export const users = [
+    {
+        id: 1,
+        user:'B47M4N',
+        pass:'w4yn3t3ch',
+        email:'bruce@waynetech.com',
+        data:{},
+    },
+    {
+        id: 2,
+        user: 'spidey',
+        pass: 'friendlyneighbor',
+        email:'parker.peter@gmail.com',
+        data:{},
+    },
+    {
+        id: 3,
+        user: 'D14N4',
+        pass: 'Amazons',
+        email: 'prince.diana@hotmail.com',
+        data:{},
+    },
+    {
+        id: 4,
+        user: 'kpride',
+        pass: 'I4mfromx-men',
+        email:'lil.kitten@gmail.com',
+        data:{},
+    },
+    {
+        id: 5,
+        user: 'ghul',
+        pass: 'worldomination',
+        email:'raz@hotmail.com',
+        data:{},
+    },
+]
