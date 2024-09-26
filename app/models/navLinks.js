fdf76714db264403510db9527a264b9e78be8bce
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-26 20:11:11 -0300

    crear links para navegacion

---
+++ app/models/navLinks.js
@@ -0,0 +1,6 @@
+export const navLinks = [
+    { text: 'Inicio',    link:'/' },
+    { text: 'Usuarios',  link:'/users' },
+    { text: 'Productos', link:'/products' },
+    { text: 'Contacto',  link:'/contact' },
+]
