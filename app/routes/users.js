30ac19343727eca26fe3947b7f5e509d81d31223
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-26 20:10:22 -0300

    app/controllers/users.js

--- app/routes/users.js
+++ app/routes/users.js
@@ -5,7 +5,7 @@ import * as usr from "../controllers/users.js";
 const usersRouter = Router()
 // Definicion de Rutas
 usersRouter.post('/', usr.createUser)      // CREATE
-usersRouter.get('/', usr.getUsers)         // READ
+usersRouter.get('/:id?', usr.getUsers)     // READ
 usersRouter.put('/:id', usr.updateUser)    // UPDATE
 usersRouter.delete('/:id', usr.deleteUser) // DELETE
 // Exportacion de Rutas
