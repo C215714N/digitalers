f3a34b96049194c87a9f66c399596adb438f2be7
Cristian Racedo <cristiandracedo@hotmail.com>
2024-09-26 20:11:44 -0300

    generar vistas parciales y distribucion de contenido

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ import 'dotenv/config'; // Lectura de archivo .env
 import morgan from 'morgan'; // Registro de solicitudes HTTP
 import express from 'express'; // Framework de enrutamiento
 import usersRouter from './app/routes/users.js';
+import { navLinks } from './app/models/navLinks.js';
+import { users } from './app/models/userModel.js';
+
 // Declaraciones
 const { PORT, HOST, DB, APP } = process.env
 const app = express()
 // Configuracion
 app.use(morgan('tiny'))
 app.use(express.urlencoded({extended: false}))
+app.set("view engine","pug");
 // Rutas
 app.use('/users', usersRouter)
+app.use("/", (_,res) => res.render('index',{ APP, navLinks, users }))
+
 // Ejecucion
 app.listen(PORT ?? 3000, () => console.log("service running on port "+PORT))
