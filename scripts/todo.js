4e476bc771a07b327839b7f506990f0336c86127
Cristian Racedo <CristianDRacedo@hotmail.com>
2025-07-30 20:35:16 -0300

    funciones y eventos de lista de tareas

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,30 +1,58 @@
-// Captura de elementos
-let todos = [];
+// Declaraciones: Constantes y Variables del Script
+    // Variables de Control
+    const todos = [];
+    let isEdit = false;
+    let current = 0;
+    // Captura de elementos:
+    const input = document.querySelector("#todo_list input");
+    const btnAdd = document.querySelector('#todo_list button');
 
+// Funciones: Bloques de codigo que pueden ser invocados y normalmente reciben argumentos
 function addTodo(text){
     const newItem = text.trim();
-    if (newItem) todos.push(newItem);
-    render();
+    const exists = todos.find(item => item == newItem);
+    if (newItem && !exists) todos.push(newItem);
 }
 function deleteTodo(index){
-    if (index > 0 && index < todos.length) 
+    if (index >= 0 && index < todos.length) 
     todos.splice(index,1);
-    render();
 }
 function changeTodo(text, index){
     todos[index] = text;
-    render();
 }
 function render(){
     const todoList = document.querySelector("#todo_list ul");
     todoList.innerHTML = "";
-    todos.forEach( function(item){
+    todos.forEach(function(item, index){
         const li = document.createElement('li');
         li.innerHTML = `
             ${item} 
-            <button class="btn p-1 r-1 bg-none b-info text-info">Editar</button>
-            <button class="btn p-1 r-1 bg-none b-danger text-danger">Eliminar</button>
+            <button class="btn p-1 r-1 bg-none b-info text-info" onclick="editBtn(${index})">Editar</button>
+            <button class="btn p-1 r-1 bg-none b-danger text-danger" onclick="removeBtn(${index})">Eliminar</button>
         `;
         todoList.append(li);
     })
+}
+// Eventos: Acciones realizadas por el usuario que desencadenan la ejecucion de instrucciones
+btnAdd.onclick = () => {
+    const text = input.value;
+    !isEdit ? addTodo(text) : changeTodo(text, current); // Ternario: expression ? if_true : if_false
+    clearInput();
+    render();
+}
+function removeBtn(index){
+    const ok = confirm("¿Desea eliminar la tarea? \n" + todos[index]);
+    if (ok) deleteTodo(index);
+    render();
+}
+function editBtn(index){
+    isEdit = true;
+    current = index;
+    input.value = todos[current];
+    input.focus();
+}
+function clearInput () {
+    isEdit = false;
+    input.value = "";
+    input.focus();
 }
