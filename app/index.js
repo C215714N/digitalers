a896e8bac13592862c51139ec15c37f14a677c7d
Cristian Racedo <CristianDRacedo@hotmail.com>
2025-08-13 21:14:19 -0300

    manejar de evento click sobre hipervinculos

--- app/index.js
+++ app/index.js
@@ -1,10 +1,15 @@
+import {HOST, method, navBarList} from "./declarations.js"
+import NavBar from "./nav.js";
 import getData from "./callback.js";
 import handleRender from "./render.js";
 
-let method = "GET";
-let url = "/assets/content/methods"
-
-getData({
-    method, url, 
-    callBack: handleRender
+NavBar(navBarList);
+document.addEventListener('click', (ev) => {
+    ev.preventDefault()
+    if (ev.target.tagName === "A" && ev.target.pathname != "/")
+        getData({
+            method,
+            url: HOST + ev.target.pathname,
+            callBack: handleRender
+        })
 })
