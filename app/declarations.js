2f8dda39832bc5bb29cf607cd7253587ebb057af
Cristian Racedo <CristianDRacedo@hotmail.com>
2025-08-13 21:11:08 -0300

    declaracion de constantes globales

---
+++ app/declarations.js
@@ -0,0 +1,17 @@
+export const 
+    method = "GET", 
+    HOST = "/assets/content/",
+    navBarList = [
+    {
+        text: "Metodos",
+        href: "/methods"
+    },
+    {
+        text: "Estados",
+        href: "/states"
+    },
+    {
+        text: "Codigos",
+        href: "/status"
+    },
+]
