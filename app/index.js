11413ca34ddd78e0623cc7ad37a3caf72000efff
Cristian Racedo <CristianDRacedo@hotmail.com>
2025-08-20 17:33:34 -0300

    manipulacion de eventos y reestructuracion

--- app/index.js
+++ app/index.js
@@ -1,15 +1,22 @@
-import {HOST, method, navBarList} from "./declarations.js"
+import {method, HOST, navBarList} from "./declarations.js"
 import NavBar from "./nav.js";
 import getData from "./callback.js";
 import handleRender from "./render.js";
 
-NavBar(navBarList);
-document.addEventListener('click', (ev) => {
-    ev.preventDefault()
-    if (ev.target.tagName === "A" && ev.target.pathname != "/")
-        getData({
-            method,
-            url: HOST + ev.target.pathname,
-            callBack: handleRender
-        })
+document.addEventListener('DOMContentLoaded', () => {
+    NavBar(navBarList);
+
+    root.addEventListener('click', (ev) => {
+        const { target } = ev
+        const { tagName: tag, pathname:path } = target
+        ev.preventDefault()
+        if (tag === "A"){
+            const type = target.dataset.type || "default";
+            getData({
+                method,
+                url: HOST[type] + path,
+                callBack: handleRender
+            })
+        }
+    }) 
 })
