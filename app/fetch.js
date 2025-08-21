e6c1f038e404c38226f0501424aadd38328761eb
Cristian Racedo <CristianDRacedo@hotmail.com>
2025-08-20 23:05:53 -0300

    verificar contenido de cabecera http

--- app/fetch.js
+++ app/fetch.js
@@ -1,12 +1,13 @@
-async function getData(url = "", options = {}) {
+async function getData({url = "", ...options}) {
     try {
         const response = await fetch(url, options);
-        if (/json/.test(response.type)) return await response.json();
-        if (/html|text|xml/.test(response.type)) return await response.text();
-        const data = await response.blob();
-        return URL.createObjectURL(data);
+        const type = options.headers["Content-Type"] || "";
+        if (/json/.test(type)) return await response.json();
+        if (/text/.test(type)) return await response.text();
+        return await response.blob();
     }
     catch (e){
         console.error("Error:", e);
     }
 }
+export default getData;
