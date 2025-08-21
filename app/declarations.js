211377d51de14ada7671774a1bcf9a37afc36132
Cristian Racedo <CristianDRacedo@hotmail.com>
2025-08-20 23:05:02 -0300

    reorganizar declaraciones globales

--- app/declarations.js
+++ app/declarations.js
@@ -5,40 +5,17 @@ export const
         remote: "https://jsonplaceholder.typicode.com/",
         default: ""
     },
+    CONTENT = {
+        local: "text/html",
+        remote: "application/json",
+        default: "text/plain"
+    },
     navBarList = [
-    {
-        text: "Metodos",
-        path: "/methods",
-        type: "local"
-    },
-    {
-        text: "Estados",
-        path: "/states",
-        type: "local"
-    },
-    {
-        text: "Codigos",
-        path: "/status",
-        type: "local"
-    },
-    {
-        text: "Error",
-        path: "/error",
-        type: "local"
-    },
-    {
-        text: "Usuarios",
-        path: "/users",
-        type: "remote"
-    },
-    {
-        text: "Publicaciones",
-        path: "/posts",
-        type: "remote"
-    },
-    {
-        text: "Galeria",
-        path: "/photos",
-        type: "remote"
-    }
-]
+        { text: "Metodos", path: "methods", type: "local" },
+        { text: "Estados", path: "states", type: "local" },
+        { text: "Codigos", path: "status", type: "local" },
+        { text: "Error", path: "error", type: "local" },
+        { text: "Usuarios", path: "users", type: "remote" },
+        { text: "Publicaciones", path: "posts", type: "remote" },
+        { text: "Galeria", path: "albums", type: "remote" }
+    ]
