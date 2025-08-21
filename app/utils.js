6451c415b75c4fdf5abf83d13e863da33b3e1d93
Cristian Racedo <CristianDRacedo@hotmail.com>
2025-08-21 00:24:55 -0300

    implementar funciones de utilidad

---
+++ app/utils.js
@@ -0,0 +1,16 @@
+import { User } from "./components/User.js";
+import { Post } from "./components/Post.js";
+import { Album } from "./components/Album.js";
+
+export const getComponent = (path) => {
+switch (path) {
+    case "/users": return User;
+    case "/posts": return Post;
+    case "/albums": return Album;
+}}
+export const getSection = () => {
+    const section = root.querySelector("section") || document.createElement("section");
+    section.innerHTML = "";
+    root.append(section);
+    return section;
+}
