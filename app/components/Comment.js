f02fcbbd0f69003192717a9527c1cee7e7cbed10
Cristian Racedo <CristianDRacedo@hotmail.com>
2025-08-22 19:51:21 -0300

    crear componente para comentarios

---
+++ app/components/Comment.js
@@ -0,0 +1,12 @@
+export const Comment = ({id, name, email, body}) => `
+    <li id="comment-${id}">
+        <p>
+            <strong>${name}</strong>
+            <em>${body}</em>
+        </p>
+        <p>
+            Publicado por 
+            <a href="mailto:${email}">${email}</a>
+        </p>
+    </li>
+`
