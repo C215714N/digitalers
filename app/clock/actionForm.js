c30d271b2dc99a18608acbe3eee22e88f3f9df4f
Cristian Racedo <CristianDRacedo@hotmail.com>
2025-09-03 18:47:08 -0300

    actualizar evento input en formulario

--- app/clock/actionForm.js
+++ app/clock/actionForm.js
@@ -4,17 +4,19 @@ export function actionForm(clock){
     const form = create({tag: "div", id:"clock-form"})
     const inputProps = {
         tag: "input",
-        type: "number"
+        type: "number",
+        min: 0,
+        max: 59
     }
-    const HHInput = create({...inputProps, onclick: ({target}) => {
+    const HHInput = create({...inputProps, max:24, oninput: ({target}) => {
         clock.HH = target.value;
         renderTime(clock);
     }})
-    const mmInput = create({...inputProps, onclick: ({target}) => {
+    const mmInput = create({...inputProps, oninput: ({target}) => {
         clock.mm = target.value;
         renderTime(clock);
     }})
-    const ssInput = create({...inputProps, onclick: ({target}) => {
+    const ssInput = create({...inputProps, oninput: ({target}) => {
         clock.ss = target.value;
         renderTime(clock);
     }})
