080b3d5b85009d9e22708436c038074d645c713b
Cristian Racedo <CristianDRacedo@hotmail.com>
2025-09-05 18:50:02 -0300

    corregir seleccion de color de piezas

--- app/checkers/Game.js
+++ app/checkers/Game.js
@@ -9,7 +9,7 @@ export class Game {
     this.board = Array.from({length: 8}, (_, rowId) => 
         Array.from({length: 8}, (_, colId) => 
             (rowId < 3 || rowId > 4) && (rowId % 2 == colId % 2) ? 
-            new Piece(rowId, colId, this.players[rowId < 3]) : null
+            new Piece(rowId, colId, this.players[Number(rowId < 3)].color) : null
         ) )
     }
     addPlayer(player){
