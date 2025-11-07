9dd2f36281bcdcf2dac2f660e6790ec89f3bf23c
Cristian Racedo <CristianDRacedo@Hotmail.com>
2025-11-07 18:56:17 -0300

    agregar: componente de clase Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import { Component } from 'react';
+
+export class Header extends Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+    return(
+        <header>
+            <h1>{this.props.title}</h1>
+            <p>{this.props.subtitle}</p>
+        </header>
+    )}
+    shouldComponentUpdate(){
+
+    }
+    componentDidUpdate(){
+    }
+    componentDidMount(){
+
+    }
+    componentWillUnmount(){
+
+    }
+}
