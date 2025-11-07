ac53da2f3a083c24633109f94bcee5385c8384c8
Cristian Racedo <CristianDRacedo@Hotmail.com>
2025-11-07 19:54:36 -0300

    revertir: "agregar: componente funcion lambda Button.jsx"
    This reverts commit 22b677098be160db4b395a0b059c7f4f3588a63f.

--- src/components/Button.jsx
+++
@@ -1,6 +0,0 @@
-export const Button = ({variant, size, text, onAction}) => 
-    <button 
-        className={'btn btn-'+variant+" " +size && size } 
-        onClick={onAction}>
-        {text}
-    </button>
