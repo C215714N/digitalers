233a7700804ae726e6937becbc92d659922488c2
Cristian Racedo <CristianDRacedo@hotmail.com>
2026-07-29 21:44:43 -0300

    ejemplo cajero automatico con bucles

--- scripts/do_while.js
+++ scripts/do_while.js
@@ -1,4 +1,7 @@
 if (confirm('¿deseas acceder a tu cuenta bancararia en DIGIBANK?')) {
+    var SALDO = 1000;
+    var DESCUBIERTO = 1000;
+    // Se ejecuta una vez y se repite MIENTRAS se cumpla la condicion
     do {
         var operacion = prompt(`
         BIENVENIDO A DIGIBANK
@@ -7,27 +10,37 @@ if (confirm('¿deseas acceder a tu cuenta bancararia en DIGIBANK?')) {
             2) DESPOSITAR
             3) EXTRAER
             4) SALIR
-        `);
+        `).toUpperCase();
         switch(operacion){
             case "1":
             case "CONSULTAR":
-                resultado = "su saldo actual es de ..."
+                resultado = `su saldo actual es de $ ${SALDO}`
             break;
             case "2":
             case "DEPOSITAR":
-                resultado = "cuanto desea ingresar..."
+                MONTO = parseFloat(prompt("¿cuanto desea ingresar?"));
+                if (!isNaN(MONTO)){
+                    SALDO+= MONTO
+                    resultado = `El deposito de $ ${MONTO} se realizo exitosamente.`;
+                }
+                else resultado = `El monto ingresado NO ES VALIDO, intente nuevamente.`
             break;
             case "3":
             case "EXTRAER":
-                resultado = "cuanto desea retirar..."
+                MONTO = parseFloat(prompt("¿Cuanto desea retirar?"));
+                if (MONTO <= (SALDO + DESCUBIERTO)) {
+                    SALDO -= MONTO;
+                    resultado = `Ya puede retirar el monto de $ ${MONTO}, quedan $ ${SALDO}.`
+                    if (SALDO < 0) resultado += " Recuerde cubrir el monto descubierto para evitar recargos"
+                }
+                else resultado = `No se pudo realizar la operacion. Corrobore que el monto ingresado no sea mayor a $ ${SALDO}`
             break;
             case "4":
             case "SALIR":
-                resultado = "gracias por utilizar nuestros servicios, DIGIBANK le desea un feliz dia"
+                resultado = "¡¡Gracias por utilizar nuestros servicios, \nDIGIBANK le desea un Feliz Dia!!"
             break;
             default:
-                resultado = "la opcion elegida NO ES VALIDA, intente nuevamente"
-
+                resultado = "La opcion elegida NO ES VALIDA, intente nuevamente"
         }
         alert(resultado);
     } while (operacion != "4" &&  operacion != "SALIR")
