7df3d571bfe0850bb28b5f1e3aabd9cf34ea829e
Cristian Racedo <CristianDRacedo@hotmail.com>
2026-08-10 20:49:03 -0300

    operacion condiciones y funciones de calculadora

--- scripts/calc.js
+++ scripts/calc.js
@@ -8,17 +8,30 @@ const calc = {
     numA: '',
     numB: '',
     op: '',
-    result: ''
+    result: '',
 }
-const isButton = ({tagName}) => tagName === "BUTTON"
+const isButton = ({tagName}) => tagName === "BUTTON";
 const renderButtons = (data, parent) => data.forEach((item) => 
     parent.innerHTML+=`<button>${item}</button>`
-)
-current = () => calc.op != '' ? "numB" : "numA";
-addNumber = (n) => calc[current()] += n;
-renderNumber = () => viewer.value = calc[current()]
-clearNumber = () => calc[current()] = calc[current()].slice(0, -1);
-clearEverything = () => calc[current()] = ""
+);
+const current = () => {
+    const { op, result } = calc
+    return result ? "result" : op ? "numB" : "numA";
+}
+const addNumber = (n) => 
+    calc[current()] += n;
+const setOperation = (symbol) => calc.op = symbol
+const renderNumber = () =>
+    viewer.value = calc[current()]
+const clearNumber = () => 
+    calc[current()] = calc[current()].slice(0, -1);
+const clearEverything = () => 
+    Object.keys(calc).forEach((k) => calc[k] = '');
+const calcOperation = () => {
+    const { numA, numB, op } = calc;
+    calc.result = eval(`${numA} ${op} ${numB}`);
+    calc.numA = calc.result
+}
 
 function renderSection([id, data]){
     const eval = typeof data === "object";
@@ -30,6 +43,23 @@ function renderSection([id, data]){
 }
 Object.entries(content).forEach((el) => renderSection(el))
 numbers.addEventListener('click', ({target}) => {
-    isButton(target) && addNumber(target.innerText);
+if (isButton(target)) {
+    const {innerText:n} = target;
+    (n >= 0 && n <= 9) && addNumber(n);
     renderNumber();
-})
+}})
+symbols.addEventListener('click', ({target}) => {
+if (isButton(target)){
+    const {innerText:op} = target;
+    const calcs = content.symbols.slice(0, -1);
+    calcs.map(s => op === s && setOperation(s));
+    op === "=" && calcOperation();
+    renderNumber();
+}})
+actions.addEventListener('click', ({target}) => {
+if (isButton(target)) {
+    const { innerText:txt } = target;
+    txt === "CE" && clearEverything();
+    txt === "C" && clearNumber();
+    renderNumber();
+}})
